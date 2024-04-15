eabe9ff7c35542218d2aaede0b60c5711c22969a
ahammed shefin <ahammedshefin1@gmail.com>
2024-04-15 12:32:18 +0530

    changed cors policy /

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const connectdb = require("./Utility/DataBase");
 const bodyParser = require('body-parser'); 
 
 const corsOption = {
-    origin: "http://localhost:5173",
+    origin: "https://qimk.pages.dev/",
     methods: "GET, POST, PUT, DELETE, PATCH ,HEAD",
     Credentials: true,
 };
